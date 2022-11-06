aa2e04b201bbaf5a8e23cd54c224cbad4aa4dc91
Antar Das <antardas2334@gmail.com>
2022-11-07 02:15:51 +0600

    :hammer: add Server Configuration

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -1,73 +1,84 @@
 import {
-  Application,
-  json,
-  urlencoded,
-  Response,
-  Request,
-  NextFunction,
-} from 'express';
-import http from 'http';
-import cors from 'cors';
-import helmet from 'helmet';
-import hpp from 'hpp';
-import cookieSession from 'cookie-session';
-import HTTP_STATUS from 'http-status-codes';
-import 'express-async-errors';
-import compression from 'compression';
+	Application,
+	json,
+	urlencoded,
+	Response,
+	Request,
+	NextFunction,
+} from "express";
+import http from "http";
+import cors from "cors";
+import helmet from "helmet";
+import hpp from "hpp";
+import cookieSession from "cookie-session";
+import HTTP_STATUS from "http-status-codes";
+import "express-async-errors";
+import compression from "compression";
+
+const SERVER_PORT = 5000;
 
 export class ChattyServer {
-  private app: Application;
+	private app: Application;
 
-  constructor(app: Application) {
-    this.app = app;
-  }
+	constructor(app: Application) {
+		this.app = app;
+	}
 
-  public start(): void {
-    this.securityMiddleware(this.app);
-    this.standardMiddleware(this.app);
-    this.routesMiddleware(this.app);
-    this.globalErrorHandler(this.app);
-    this.startSever(this.app);
-    // this.createSocketIO(this.app)
-    // this.startHttpServer(this.app)
-  }
+	public start(): void {
+		this.securityMiddleware(this.app);
+		this.standardMiddleware(this.app);
+		this.routesMiddleware(this.app);
+		this.globalErrorHandler(this.app);
+		this.startSever(this.app);
+	}
 
-  // ** Middleware **
+	// ** Middleware **
 
-  private securityMiddleware(app: Application): void {
-    this.app.use(
-      cookieSession({
-        name: 'session',
-        keys: ['test1', 'test2'],
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-        secure: false, // NOTE: set to true in production
-      })
-    );
-    this.app.use(hpp());
-    this.app.use(helmet());
-    this.app.use(
-      cors({
-        origin: '*', // NOTE: set the client origin in production
-        credentials: true, // if true, the server will accept the cookie
-        optionsSuccessStatus: HTTP_STATUS.OK,
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      })
-    );
-  }
+	private securityMiddleware(app: Application): void {
+		this.app.use(
+			cookieSession({
+				name: "session",
+				keys: ["test1", "test2"],
+				maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+				secure: false, // NOTE: set to true in production
+			})
+		);
+		this.app.use(hpp());
+		this.app.use(helmet());
+		this.app.use(
+			cors({
+				origin: "*", // NOTE: set the client origin in production
+				credentials: true, // if true, the server will accept the cookie
+				optionsSuccessStatus: HTTP_STATUS.OK,
+				methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+			})
+		);
+	}
 
-  private standardMiddleware(app: Application): void {
-    this.app.use(compression());
-    app.use(json({ limit: '50mb' }));
-    app.use(urlencoded({ extended: true, limit: '50mb' }));
-  }
-  private routesMiddleware(app: Application): void {}
+	private standardMiddleware(app: Application): void {
+		this.app.use(compression());
+		app.use(json({ limit: "50mb" }));
+		app.use(urlencoded({ extended: true, limit: "50mb" }));
+	}
+	private routesMiddleware(app: Application): void {}
 
-  // ** When any error occur it's send to the client side
-  private globalErrorHandler(app: Application): void {}
+	// ** When any error occur it's send to the client side
+	private globalErrorHandler(app: Application): void {}
 
-  private startSever(app: Application): void {}
+	private async startSever(app: Application): Promise<void> {
+		try {
+			const httpServer: http.Server = new http.Server(app);
+			this.startHttpServer(httpServer);
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-  private createSocketIO(httpServer: http.Server): void {}
+	private createSocketIO(httpServer: http.Server): void {}
 
-  private startHttpServer(httpServer: http.Server): void {}
+	private startHttpServer(httpServer: http.Server): void {
+		httpServer.listen(SERVER_PORT, () => {
+			console.log(`Server is running on port ${SERVER_PORT}`);
+		});
+	}
 }
