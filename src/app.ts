68a9a0482034ec52d7fc3a85e8d968e6648bf59b
Antar Das <antardas2334@gmail.com>
2022-11-09 21:44:58 +0600

    :wrench: adding config or environment variable

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import express, { Express } from "express";
 import { ChattyServer } from "./setupServer";
 import connectDB from "./setupDatabase";
-
+import { config } from "./config";
 class Application {
 	public initialize(): void {
+		this.loadConfig();
 		connectDB();
 		const app: Express = express();
 		const server: ChattyServer = new ChattyServer(app);
 		server.start();
+	}
+
+	private loadConfig(): void {
+		config.validateConfig();
 	}
 }
 
